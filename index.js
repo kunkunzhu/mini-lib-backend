31a39283ffa30332c50632f6358917f57e6eee59
kunkunzhu <zkzhu@uwaterloo.ca>
2021-12-23 17:15:14 -0500

    ;_;

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+/* eslint-disable */
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const Book = require('./models/book')
 
+// MIDDLEWARES
+// (the execution order of middleware is the same as the order that they are loaded into express)
+
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 
-/* middleware - request logging */
+/* request logging */
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:  ', request.path)
@@ -14,44 +24,49 @@ const requestLogger = (request, response, next) => {
   
 app.use(requestLogger)
 
-app.use(express.static('build'))
-
-/* middleware - allow request from all origins */
-const cors = require('cors')
-const { response } = require('express')
-
-app.use(cors())
+/* error handling */
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+}
 
 /* initial books */
 let books = [
     {
         id: 1,
-        title: "This One Wild and Precious Life",
-        author: "Sarah Wilson",
+        title: 'This One Wild and Precious Life',
+        author: 'Sarah Wilson',
         progress: 2
     },
     {
         id: 2,
-        title: "User Friendly",
-        author: "Cliff Kuang & Robert Fabricant",
+        title: 'User Friendly',
+        author: 'Cliff Kuang & Robert Fabricant',
         progress: 2
     },
     {
         id: 3,
-        title: "Do Not Say We Have Nothing",
-        author: "Madeleine Thien",
+        title: 'Do Not Say We Have Nothing',
+        author: 'Madeleine Thien',
         progress: 3
     },
     {
         id: 4,
-        title: "Little Fires Everywhere",
-        author: "Celeste Ng",
+        title: 'Little Fires Everywhere',
+        author: 'Celeste Ng',
         progress: 3
     },
     {
         id: 5,
-        title: "The Defining Decade",
-        author: "Meg Jay",
+        title: 'The Defining Decade',
+        author: 'Meg Jay',
         progress: 1
     }
 ]
@@ -59,66 +74,62 @@ let books = [
 /* creating a unique ID */
 const generateId = () => {
     const maxId = books.length > 0
-      ? Math.max(...books.map(n => n.id))
-      : 0
+        ? Math.max(...books.map(n => n.id))
+        : 0
     return maxId + 1
-  }
+}
 
 // API CALLS
 
 /* finding a specific book */
-app.get('/api/books/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const book = books.find(b => b.id === id)
-    response.json(book)
-
-    if (book) {
-        response.json(book)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/books/:id', (request, response, next) => {
+    Book.findById(request.params.id)
+        .then(book => {
+            if (book) {
+                response.json(book)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 /* deleting a specific book */
-app.delete('/api/books/:id', (request, response) => {
-    const id = Number(request.params.id)
-    books = books.filter(b => b.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/books/:id', (request, response, next) => {
+    Book.findByIdAndRemove(request.params.id)
+        .then(result => {
+            console.log(result)
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
   
 /* getting all books */
-app.get('/api/books', (response) => {
-    response.json(books)
+app.get('/api/books', (request, response) => {
+    Book.find({}).then(books => {
+        response.json(books)
+    })
 })
 
 /* change the progress of a book */
-app.put('/api/books/:id', (request, response) => {
-    console.log("HELLO")
+app.put('/api/books/:id', (request, response, next) => {
     const body = request.body
-    
+  
     const book = {
         title: body.title,
         author: body.author,
         progress: body.progress,
-        id: body.id,
     }
-
-    console.log(book)
-
-    for (let i = 0; i < books.length; i++) {
-        if (books[i].id == id) {
-            books[i] = book;
-        }
-    }
-    
-    books.id = body.progress;
-
-    response.json(book)
+  
+    Book.findByIdAndUpdate(request.params.id, book, { new: true })
+        .then(updatedBook => {
+            response.json(updatedBook)
+        })
+        .catch(error => next(error))
 })
 
 /* creating a new book */
-app.post('/api/books', (request, response) => {
+app.post('/api/books', (request, response, next) => {
     console.log(request.body)
     const body = request.body
 
@@ -132,25 +143,31 @@ app.post('/api/books', (request, response) => {
     for (let i = 0; i < books.length; i++) {
         if (books[i].title === book.title && 
             books[i].author === book.author) {
-                return response.status(400).json({ 
-                    error: 'this book already exists' 
-                })
-            }
+            return response.status(400).json({ 
+                error: 'this book already exists' 
+            })
+        }
     }
 
-    books = books.concat(book)
-
-    response.json(book)
+    book
+    // example of promise chaining
+        .save()
+        .then(savedBook => savedBook.toJSON())
+        .then(savedAndFormattedBook => {
+            response.json(savedAndFormattedBook)
+        })
+        .catch(error => next(error))
 })
 
-
-const unknownEndpoint = (response) => {
+/* unknown endpoint */
+const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-  
-app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+app.use(unknownEndpoint)
+app.use(errorHandler)  // last loaded middleware
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
---
+++ models/books.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://zkzhu:books@cluster0.djzz2.mongodb.net/mini-lib?retryWrites=true`
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+})
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    progress: Number
+})
+
+bookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
